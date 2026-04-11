938395eca8d9d6317a443bbbaa06be63395e09c1
Scriptify Hub <63516612+ScriptifyHub@users.noreply.github.com>
2026-04-11 03:15:15 -0400

    Update bgsi.lua

--- bgsi.lua
+++ bgsi.lua
@@ -2,8 +2,19 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local Lib = {}
 
-function Lib.test()
-    print('works')
+function Lib.createWindow()
+    local success, result = pcall(function()
+        return UserInterface:CreateWindow({Name = 'Bubble Gum Simulator Infinity',Icon = 0,LoadingTitle = 'Thank you for chosing Scriptify.',LoadingSubtitle = 'Your menu should load shortly.',ShowText = 'ShowText test',Theme = 'Default',ToggleUIKeybind = 'K',DisableRayfieldPrompts = true,DisableBuildWarnings = false,ConfigurationSaving = {Enabled = true,FolderName = nil,FileName = 'Big Hub'},Discord = {Enabled = false,Invite = 'noinvitelink',RememberJoins = true},KeySystem = false,KeySettings = {Title = 'Key System Title',Subtitle = 'Subtitle',Note = 'No method of obtaining the key is listed.',FileName = 'Key',SaveKey = false,GrabKeyFromSite = false,Key = {'Hello1', 'Hello2'}}})
+    end)
+    if not success then
+        return false
+    else
+        return true
+    end
+end
+
+function Lib.notifyUser(UserInterface, title, content, duration image)
+    UserInterface:Notify({Title = title, Content = content, Duration = duration, Image = image})
 end
 
 function Lib.claimAllPlaytime()
